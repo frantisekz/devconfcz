8a6afc8f7aeb1be2605c647c72c392df10a1e42d
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2016-09-05 15:26:52 +0200

    Make js code around navbar more robust

--- web/js/devconf.js
+++ web/js/devconf.js
@@ -4,12 +4,21 @@
   // hide .navbar first
   $(".navbar").hide();
 
-  // fade in .navbar
+  // get logo distance from top
+  logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
+
+  // user can load already scrolled page (eg. refreshing, ...); check it even on document ready
+  if ($(this).scrollTop() > logo_pos) {
+    $('.navbar').fadeIn();
+  } else {
+    $('.navbar').fadeOut();
+  }
+
+  // hook on scroll and show/hide navbar
   $(function () {
     $(window).scroll(function () {
       // set distance user needs to scroll before we fadeIn navbar
       // FIXME: Make this START at the ABOUT page
-      logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
       if ($(this).scrollTop() > logo_pos) {
         $('.navbar').fadeIn();
       } else {
